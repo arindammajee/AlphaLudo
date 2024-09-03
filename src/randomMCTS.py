fc7e1383ad376def3ef8442a888a41a1236bcd5f
Arindam Majee <arimajee@amazon.com>
2024-09-04 01:49:22 +0530

    Revert "Add run time list"
    This reverts commit a2f3d28cadc9d1af9f24436743c744183ee4b5ae.

--- src/randomMCTS.py
+++ src/randomMCTS.py
@@ -75,7 +75,7 @@ def simulate_mcts_experiment(TOTAL_GAMES, MCTS_RUN_TIME=0.01, MCTS_ITERATIONS=10
 
 
 if __name__ == '__main__':
-    mcts_run_time_list = [0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1,
+    mcts_run_time_list = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1,
                           0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     total_games = 1000
     wandb.init(project="MCTS-LUDO",
